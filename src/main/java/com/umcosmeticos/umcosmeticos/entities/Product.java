a26d5bee79dc5e29fe21ceb0462e7d3e7fd7e5a5
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-02-24 22:36:42 -0300

    :sparkles: feat: product e category

---
+++ src/main/java/com/umcosmeticos/umcosmeticos/entities/Product.java
@@ -0,0 +1,82 @@
+package com.umcosmeticos.umcosmeticos.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String descriptiom;
+    private double price;
+    private String imgUrl;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+    joinColumns = @JoinColumn(name = "product_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String descriptiom, double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.descriptiom = descriptiom;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptiom() {
+        return descriptiom;
+    }
+
+    public void setDescriptiom(String descriptiom) {
+        this.descriptiom = descriptiom;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+}
