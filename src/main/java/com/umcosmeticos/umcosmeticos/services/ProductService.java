108a489a887d537a01b690a4695b50a5688b0d29
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-02-28 09:55:51 -0300

    :sparkles: feat: Insert Product

--- src/main/java/com/umcosmeticos/umcosmeticos/services/ProductService.java
+++ src/main/java/com/umcosmeticos/umcosmeticos/services/ProductService.java
@@ -4,9 +4,12 @@ import com.umcosmeticos.umcosmeticos.dto.ProductDTO;
 import com.umcosmeticos.umcosmeticos.entities.Product;
 import com.umcosmeticos.umcosmeticos.repositorys.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,6 +26,24 @@ public class ProductService {
 
         ProductDTO productDTO = new ProductDTO(product);
         return productDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(product -> new ProductDTO(product));
+    }
+
+    @Transactional()
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
     }
 
 }
